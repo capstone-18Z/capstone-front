aa562f78bc8d9ba17e7ba8a024e0b96910b1b3ae
BaekRaven <yjw70809@gmail.com>
2023-02-25 15:31:12 +0900

    팀 빌딩 폼 구현
    기본적인 폼만 구현.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Link} from "react-router-dom";
+import Index from './component/teambuildingform';
 
-function App() {
-  return (
+function App() {    
+
+      const onSubmitHandler = (e) => {
+        e.preventDefault();
+        const name = e.target.text.value;
+        fetch('/members/new',{
+          method: 'POST',
+          headers: {
+            'Content-Type' : 'application/json',
+          },
+            body: JSON.stringify({ 
+            name,
+          }),
+        });
+      };
+
+
+
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>	
+      <p><Link to="/index">팀빌딩하기</Link></p>
+      <p><Link to="/index2">팀구하기</Link></p>
+				<Routes>          
+					<Route path="/index" element={<Index/>}></Route>					
+				</Routes>
+			</BrowserRouter>
     </div>
   );
 }
