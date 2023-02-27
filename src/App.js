f0e8d02ce0d67a235dbb3e159ece98d9cac24a1e
BaekRaven <yjw70809@gmail.com>
2023-02-27 15:24:36 +0900

    useState로 팀빌딩폼 구현
    useState로 값들 관리

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import React, {useState, useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {Link} from "react-router-dom";
 import Index from './component/teambuildingform';
 
-function App() {    
-
-      const onSubmitHandler = (e) => {
-        e.preventDefault();
-        const name = e.target.text.value;
-        fetch('/members/new',{
-          method: 'POST',
-          headers: {
-            'Content-Type' : 'application/json',
-          },
-            body: JSON.stringify({ 
-            name,
-          }),
-        });
-      };
-
-
-
-
+function App() { 
+  
     return (
     <div className="App">
       <BrowserRouter>	
