202ce6640a91f9d110ec8606c2726ba0b0fe5997
BaekRaven <yjw70809@gmail.com>
2023-03-04 15:01:45 +0900

    팀원 구하는 모집글들 보여주는 기능 구현중
    team.js가 팀원 구하는 모집글들을 다 보여주는 js이다.

--- src/components/TeamBuilding/index.js
+++ src/components/TeamBuilding/index.js
@@ -2,48 +2,88 @@ import "./index.css";
 import { useState } from "react";
 
 function TeamBuilding() {
-    //현재 프론트, 백 팀원수 관리
-    const [frontnumber, setfrontNumber] = useState(0);
-    const [backnumber, setbackNumber] = useState(0);
-    //프로젝트 설명과 과목 관리
+    //현재 프론트, 백 팀원수 데이터 관리
+    const [current_front_member, setfrontNumber] = useState(0);
+    const [current_back_member, setbackNumber] = useState(0);
+    //프로젝트 제목, 설명, 과목 데이터 관리
+    const [title, setTitle]=useState("");
     const [project_explan, setProject_explan]= useState("");
     const [project_subject, setProject_subject]= useState("");
-    //언어 점수 관리
+    //언어 점수 관리__
+    
     const [c,setC] = useState(1);
     const [java,setJava] = useState(1);
     const [cpp,setCpp] = useState(1);
+    const [cs, setCs] = useState(1);//c#임
+    const [python,setPython] = useState(1);
+    const [javascript,setJavascript] = useState(1);
+    const [vb, setVb]=  useState(1);//비주얼베이직
+
+    const today = new Date();
+    const now = today.toLocaleTimeString();
+
 
     const addNumber = () =>{
-        setfrontNumber(frontnumber+1);
+        setfrontNumber(current_front_member+1);
     }
     const minusNumber = () =>{
-        if(frontnumber>0){
-            setfrontNumber(frontnumber-1);
+        if(current_front_member>0){
+            setfrontNumber(current_front_member-1);
         }    
     }
     const addbackNumber = () =>{
-        setbackNumber(backnumber+1);
+        setbackNumber(current_back_member+1);
     }
     const minusbackNumber = () =>{
-        if(backnumber>0){
-            setbackNumber(backnumber-1);
+        if(current_back_member>0){
+            setbackNumber(current_back_member-1);
         }    
     }
     const PostRequest = (e) => {
         e.preventDefault();
         console.log("막음");
     }
-    const qwer =() =>{
-        console.log(project_explan+"  "+project_subject,);
+    const test = () => {
+        console.log(+c+" "+java+" "+cpp+" "+python+" "+cs+" "+vb);
+        console.log(now);
     }
+  
+ 
+    const onSubmitHandler = (e) => {
+        fetch('http://1871166.iptime.org:8080/team/new',{
+          method: 'POST',
+          headers: {
+            'Content-Type' : 'application/json',
+          },
+            body: JSON.stringify({
+                title,
+                detail : project_explan,
+                currentFrontMember: current_front_member,
+                currentBackMember: current_back_member,
+                c,
+                java,
+                cpp,
+                python,
+                cs,
+                vb,
+                createDate : now,
+                updateDate : now,
+          }),
+        });
+      };
+      //변수명 서버랑 똑같이 해야 보내짐
 
     return (
         <div className="teambuildingform">
+            
             <h1>팀 빌딩 폼</h1>
-            <form name="team-form" onSubmit={PostRequest}>                
-                <p>프로젝트 한줄 설명<input type="text" value={project_explan} onChange={(event)=>{
+            <form name="team-form" onSubmit={PostRequest}>
+                <h2>프로젝트</h2>
+                <p>제목<input type="text" value={title} onChange={(event)=>{
+                    setTitle(event.target.value); }}/></p>    
+                <p>한줄 설명<input type="text"  value={project_explan} onChange={(event)=>{
                     setProject_explan(event.target.value); }}/></p>
-                <p>프로젝트 과목 <input type="text" value={project_subject} onChange={(event)=>{
+                <p>과목 <input type="text" value={project_subject} onChange={(event)=>{
                     setProject_subject(event.target.value); }}/></p>
                 <p>언어</p>
                 <p>C언어 <input name= 'C언어' type='range' min={0}
@@ -54,10 +94,26 @@ function TeamBuilding() {
                 max={5} step={1} value={java} onChange={(event) => {
                     setJava(event.target.valueAsNumber);
                   }} /></p>
-                <p>C++ <input name= 'Java' type='range' min={0}
+                <p>C++ <input name= 'C++' type='range' min={0}
                 max={5} step={1} value={cpp} onChange={(event) => {
                     setCpp(event.target.valueAsNumber);
                   }} /></p>
+                <p>Python <input name= 'python' type='range' min={0}
+                max={5} step={1} value={python} onChange={(event) => {
+                    setPython(event.target.valueAsNumber);
+                  }}/></p>
+                <p>C# <input name= 'C#' type='range' min={0}
+                max={5} step={1} value={cs} onChange={(event) => {
+                    setCs(event.target.valueAsNumber);
+                  }}/></p>
+                <p>JavaScript <input name= 'JavaScript' type='range' min={0}
+                max={5} step={1} value={javascript} onChange={(event) => {
+                    setJavascript(event.target.valueAsNumber);
+                  }}/></p>
+                <p>Visual Basic <input name= 'Visual Basic' type='range' min={0}
+                max={5} step={1} value={vb} onChange={(event) => {
+                    setVb(event.target.valueAsNumber);
+                  }}/></p>
                 <p>선호트랙</p>
                     <input type="checkbox" id="btn1" name="checkWrap" value="웹공학트랙" />
                     <label htmlFor="btn1">웹공학트랙</label>
@@ -69,11 +125,11 @@ function TeamBuilding() {
                     <label htmlFor="btn4">디지털콘텐츠트랙</label>
                 <p>현재 팀원 수:  프론트  
                     <button onClick={addNumber}>+1</button>
-                    {frontnumber}
+                    {current_front_member}
                     <button onClick={minusNumber}>-1</button> 
                 백
                     <button onClick={addbackNumber}>+1</button>
-                    {backnumber}
+                    {current_back_member}
                     <button onClick={minusbackNumber}>-1</button>              
                     </p>
                 <p>모집하는 팀원 수</p>
@@ -81,9 +137,9 @@ function TeamBuilding() {
                 <p>모집 기간</p>
                 <p>내용 <textarea id="story" name="story"
                 rows={5} cols={33}/></p>
-                <button onClick={qwer}>등록하기</button>
+                <button onClick={onSubmitHandler}>등록하기</button>
             </form>
-        </div>        
+        </div>              
     );
 }
 export default TeamBuilding;
