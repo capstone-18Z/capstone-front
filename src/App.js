d82c784dd2b5656dd37c7e60abaffc5f80dac944
BaekRaven <yjw70809@gmail.com>
2023-04-09 01:00:56 +0900

    라우팅 수정
    라우팅 수정

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import TeamDetail from "./components/TeamDetail/teamDetail";
 import RecommendUserList from "./components/RecommendUserList/recommendUserList";
 import Team from "./components/Teams/team";
 import EditTeam from "./components/EditTeam/editTeam";
+import MyPage from "./components/MyPage/mypage";
 
 const BASE_URL = "https://port-0-capstone-back-6g2llf7te70n.sel3.cloudtype.app";
 
@@ -42,6 +43,7 @@ function App() {
         <Routes>
           <Route path="/*" element={<LoginPage />}></Route>
           <Route path="/main" element={<><Header /><MainPage /></>}></Route>
+          <Route path="/mypage" element={<><Header/><MyPage /></>}></Route>
           {/* 중첩라우팅 */}
           <Route path="/team" element={<><Header/><Team /></>}></Route>
           <Route path="/post/*" element={<><Header/><PostPage /></>}></Route>
