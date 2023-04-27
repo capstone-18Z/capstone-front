871d486dcbd40e6fb00408f87965e6d4f5d6aa7b
kys0417 <92397578+kys0417@users.noreply.github.com>
2023-04-27 22:49:22 +0900

    profile 컴포넌트 추가 (수정예정)
    profile 컴포넌트 추가 (수정예정)

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import EditProfile from "../components/EditProfile/index";
+import Profile from "../components/Profile";
 
 function ProfilePage() {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Profile/>} />
         <Route path="edit" element={<EditProfile />} />
       </Routes>
     </div>
