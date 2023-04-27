4898447c62831ed63f1238487945d23f4afdd61c
kys0417 <yungsang1111@gmail.com>
2023-04-27 22:28:58 +0900

    ListPage 추가
    ListPage 추가

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Team from "./components/Teams/team";
 import EditTeam from "./components/EditTeam/editTeam";
 import MyPage from "./components/MyPage/mypage";
 import ProfilePage from "./pages/ProfilePage";
+import ListPage from "./pages/ListPage";
 
 
 function App() {
@@ -45,7 +46,7 @@ function App() {
           <Route path="/main" element={<><Header /><MainPage /></>}></Route>
           <Route path="/mypage" element={<><Header/><MyPage /></>}></Route>
           <Route path="/profile/*" element={<><Header/><ProfilePage/></>}></Route>
-          {/* 중첩라우팅 */}
+          <Route path="/list/*" element={<><Header/><ListPage/></>}></Route>
           <Route path="/team" element={<><Header/><Team /></>}></Route>
           <Route path="/post/*" element={<><Header/><PostPage /></>}></Route>
           <Route path="/team/:teamId/" element={<><Header/><TeamDetail /></>}></Route>
