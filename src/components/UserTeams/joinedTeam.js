06cf17b4f86b289410761f3b0db054d1db04bec6
RyuSungHyeon990604 <1871166@hansung.ac.kr>
2023-06-01 19:23:59 +0900

    팀나가기 수정
    팀장일경우 리스트에서 사라지지 않게수정

--- src/components/UserTeams/joinedTeam.js
+++ src/components/UserTeams/joinedTeam.js
@@ -1,18 +1,18 @@
 import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
-import { MyTeamCard } from "./myTeamCard"
-import { useNavigate } from 'react-router-dom';
-import "./joinedTeam.css"
+import { MyTeamCard } from "./myTeamCard";
+import { useNavigate } from "react-router-dom";
+import "./joinedTeam.css";
 import Swal from "sweetalert2";
-import "../../myAlert.css"
+import "../../myAlert.css";
 
-function JoinedTeam(){
+function JoinedTeam() {
   const refresh_token = localStorage.getItem("refresh-token");
   const login_token = localStorage.getItem("login-token");
   const navigate = useNavigate();
 
-  const [joinedTeams, setJoinedTeams] = useState([]); 
-  useEffect(() => {           
+  const [joinedTeams, setJoinedTeams] = useState([]);
+  useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/teams/allMyTeams`, {
       headers: {
         "refresh-token": refresh_token,
@@ -24,39 +24,54 @@ function JoinedTeam(){
         console.log(obj);
         setJoinedTeams(obj.data);
       });
-}, []);
-  
-    const 팀나가기=(teamId)=>{
-        fetch(`${process.env.REACT_APP_API_URL}/teams/deleteMember?teamId=${teamId}&userId=${localStorage.getItem("userId")}`, {
-          method:"POST",  
-          headers: {
-              
-              "refresh-token": refresh_token,
-              "login-token": login_token,
-            },
-          })
-            .then((response) => response.json())
-            .then((obj) => {
-              console.log(obj);
-              Swal.fire({
-                title: obj.message,
-                icon: 'success',
-                customClass: {
-                  icon: 'my-custom-icon-class',
-                  actions: 'my-custom-actions-class',
-                },
-              })
-              setJoinedTeams(joinedTeams.filter(data=>{
-                return data.teamId!=teamId
-            }))
+  }, []);
+
+  const 팀나가기 = (teamId) => {
+    fetch(
+      `${
+        process.env.REACT_APP_API_URL
+      }/teams/deleteMember?teamId=${teamId}&userId=${localStorage.getItem(
+        "userId"
+      )}`,
+      {
+        method: "POST",
+        headers: {
+          "refresh-token": refresh_token,
+          "login-token": login_token,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((obj) => {
+        console.log(obj);
+        Swal.fire({
+          title: obj.message,
+          icon: "success",
+          customClass: {
+            icon: "my-custom-icon-class",
+            actions: "my-custom-actions-class",
+          },
+        });
+        if (obj.message != "팀장은 팀을 나갈 수 없습니다")
+          setJoinedTeams(
+            joinedTeams.filter((data) => {
+              return data.teamId != teamId;
+            })
+          );
       });
-    }
-    const 채팅=(data)=>{
-      window.open(`/chat?waitingId=${data.teamId}&nickname=${localStorage.getItem("nickname")}&mode=room`, "_blank", "width=450,height=650");
-    }
-    const goTeam = (link) => {
-      navigate(`/list/team/${link}`);
-  }
+  };
+  const 채팅 = (data) => {
+    window.open(
+      `/chat?waitingId=${data.teamId}&nickname=${localStorage.getItem(
+        "nickname"
+      )}&mode=room`,
+      "_blank",
+      "width=450,height=650"
+    );
+  };
+  const goTeam = (link) => {
+    navigate(`/list/team/${link}`);
+  };
 
   function isUrl(str) {
     const pattern = /^(https?:\/\/)?([a-z0-9-]+\.)+[a-z0-9]{2,4}.*$/i;
@@ -70,26 +85,42 @@ function JoinedTeam(){
       (match) => `<a href="${match}" target="_blank">${match}</a>`
     );
   }
-    
-    return(
-        <div className="joined-team-container" >
-            {joinedTeams==null ?<div>소속된 팀이 없습니다.</div> : joinedTeams.map(data => (
-                <div key={data.teamId} className="joined-team-card-ryu" > 
-                    <MyTeamCard team={data} />                    
-                   <div className="joinedTeam-card-bottom">
-                    {isUrl(data.teamURL)==true ? <span className="teamURL"
-                      dangerouslySetInnerHTML={{
-                        __html: makeLink(data.teamURL),
-                      }}
-                    /> : <span className="teamURL">{data.teamURL}</span>}
-                    <div className="joinedTeam-btn-group">
-                     <button className="chatBtn" onClick={() => 채팅(data)}>채팅</button>
-                     <button className="exitBtn" onClick={() => 팀나가기(data.teamId)}>팀나가기</button>
-                     </div>
-                     </div>
-                </div>
-            ))}
-        </div>
-    );
+
+  return (
+    <div className="joined-team-container">
+      {joinedTeams == null ? (
+        <div>소속된 팀이 없습니다.</div>
+      ) : (
+        joinedTeams.map((data) => (
+          <div key={data.teamId} className="joined-team-card-ryu">
+            <MyTeamCard team={data} />
+            <div className="joinedTeam-card-bottom">
+              {isUrl(data.teamURL) == true ? (
+                <span
+                  className="teamURL"
+                  dangerouslySetInnerHTML={{
+                    __html: makeLink(data.teamURL),
+                  }}
+                />
+              ) : (
+                <span className="teamURL">{data.teamURL}</span>
+              )}
+              <div className="joinedTeam-btn-group">
+                <button className="chatBtn" onClick={() => 채팅(data)}>
+                  채팅
+                </button>
+                <button
+                  className="exitBtn"
+                  onClick={() => 팀나가기(data.teamId)}
+                >
+                  팀나가기
+                </button>
+              </div>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 export default JoinedTeam;
